51ef7611485f237dc4b061105c53a07c2f4dff7e
Piyaush Kumar Rana <piyaush.rana@BRILLIO.COM>
2017-06-21 18:10:33 +0530

    dadadasd

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-
+app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
@@ -12,8 +12,14 @@ app.get('/about',function(req,res){
   res.sendFile(path.join(__dirname+'/about.html'));
 });
 
-app.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
+app.get('/privacy_policy',function(req,res){
+  res.sendFile(path.join(__dirname+'/privacy.html'));
+});
+app.get('/team',function(req,res){
+  res.sendFile(path.join(__dirname+'/team.html'));
+});
+app.get('/contact',function(req,res){
+  res.sendFile(path.join(__dirname+'/contact.html'));
 });
 
 app.listen(3000);
